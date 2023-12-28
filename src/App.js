b258095d06886a6a95b53df9772cb6d104cea008
ATUL LAMBE <139805659+Atullambe@users.noreply.github.com>
2023-12-28 23:40:17 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {useState} from "react";
+import "./style.css";
+
+import MovieList from "./Components/MovieList";
+
+
+const App = () => {
+    
+
+    return(
+        <div>
+            <MovieList/> 
+
+            
+        </div>
+    )
+}
+
+export default App;
